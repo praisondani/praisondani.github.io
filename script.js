89db105e3222eaba676c29e66092faf9c70a7244
dan <praisondani@gmail.com>
2023-11-15 16:10:13 -0500

    made some updates to stylesheet

--- script.js
+++ script.js
@@ -5,9 +5,21 @@ document.getElementById('contact-form').addEventListener('submit', function(even
 
 document.querySelector('.hamburger-menu').addEventListener('click', function() {
     const sidebar = document.querySelector('header');
-    if (sidebar.style.height === 'auto' || sidebar.style.height === '100%') {
-        sidebar.style.height = '0';
+    const content = document.querySelector('section');
+    if (sidebar.style.transform === 'translateX(-100%)') {
+        sidebar.style.transform = 'translateX(0)';
+        content.style.marginLeft = '200px'; // Match sidebar width
     } else {
-        sidebar.style.height = 'auto'; // Adjust to '100%' if 'auto' doesn't work
+        sidebar.style.transform = 'translateX(-100%)';
+        content.style.marginLeft = '0';
     }
 });
+
+
+document.querySelector('.toggle-sidebar').addEventListener('click', function() {
+    const sidebar = document.querySelector('header');
+    const content = document.querySelector('section');
+    sidebar.classList.toggle('collapsed-sidebar');
+    content.classList.toggle('collapsed-content');
+});
+
